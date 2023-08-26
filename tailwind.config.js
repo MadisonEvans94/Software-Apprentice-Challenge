e12ecc51c3c93df2db6417369bab7f0cefc209b0
Madison Evans <m.corbinevans@gmail.com>
2023-08-25 19:45:38 -0700

    added a theme to config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: ["./src/**/*.{js,jsx,ts,tsx}"],
+	theme: {
+		extend: {
+			colors: {
+				primary: "#d8dde6",
+				secondary: "#2576f5",
+				tertiary: "#28cdfc",
+				info: "#262626",
+			},
+		},
+	},
+	plugins: [],
 };
