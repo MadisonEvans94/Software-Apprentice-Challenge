cdd1252d0b9bd6eb230113367b194e5f276c457d
Madison Evans <m.corbinevans@gmail.com>
2023-08-25 19:21:57 -0700

    added dummy card list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,50 @@
 import "./App.css";
 import Card from "./components/Card";
+
+const cardData = [
+	{
+		campaign: "Summer Sale",
+		adset: "Social Media Ads",
+		creative: "Beach Vacation Promo",
+		spend: 1500,
+		impressions: 25000,
+		clicks: 1200,
+		results: 72,
+	},
+	{
+		campaign: "Back to School",
+		adset: "Display Ads",
+		creative: "School Supplies Deal",
+		spend: 1000,
+		impressions: 18000,
+		clicks: 800,
+		results: 38,
+	},
+	{
+		campaign: "Fall Fashion",
+		adset: "Search Ads",
+		creative: "Festive Sneak Peek",
+		spend: 2000,
+		impressions: 30000,
+		clicks: 1500,
+		results: 61,
+	},
+];
+
 function App() {
 	return (
 		<div className="App">
-			<Card />
+			{cardData.map((card) => (
+				<Card
+					campaign={card.campaign}
+					adset={card.adset}
+					creative={card.creative}
+					spend={card.spend}
+					impressions={card.impressions}
+					clicks={card.clicks}
+					results={card.results}
+				/>
+			))}
 		</div>
 	);
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,37 +1,42 @@
 import React from "react";
 
-const Card = () => {
+const Card = ({
+	campaign,
+	adset,
+	creative,
+	spend,
+	impressions,
+	clicks,
+	results,
+}) => {
 	return (
 		<div className="bg-white rounded-lg shadow-lg p-6 w-96 mx-auto my-4">
 			<div className="grid grid-cols-2 gap-4">
 				<div className="text-lg font-semibold text-blue-600 mb-4 col-span-2">
-					Campaign: Summer Sale
+					Campaign: {campaign}
 				</div>
 				<div className="text-base font-medium text-gray-600">
 					Adset:
 				</div>
-				<div className="text-base text-gray-800">Social Media Ads</div>
+				<div className="text-base text-gray-800">{adset}</div>
 				<div className="text-base font-medium text-gray-600">
 					Creative:
 				</div>
-				<div className="text-base text-gray-800">
-					Beach Vacation Promo
-				</div>
+				<div className="text-base text-gray-800">{creative}</div>
 				<div className="text-base font-medium text-gray-600">
 					Spend:
 				</div>
-				<div className="text-base text-gray-800">1500</div>
+				<div className="text-base text-gray-800">{spend}</div>
 				<div className="text-base font-medium text-gray-600">
 					Impressions:
 				</div>
-				<div className="text-base text-gray-800">25000</div>
+				<div className="text-base text-gray-800">{impressions}</div>
 				<div className="text-base font-medium text-gray-600">
 					Clicks:
 				</div>
-				<div className="text-base text-gray-800">1200</div>
+				<div className="text-base text-gray-800">{clicks}</div>
 				<div className="text-base font-medium text-gray-600 col-span-2">
-					Results: (This would be fetched from Google Analytics based
-					on matching criteria)
+					Results: {results}
 				</div>
 			</div>
 		</div>
