4b725ade018c268f990e1462fd825b98f1e8d54c
Madison Evans <m.corbinevans@gmail.com>
2023-08-27 09:54:38 -0700

    updated indexing strategy

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import Card from "./Card";
 const CardList = ({ cardData }) => {
+	console.log("CARD DATA: ", cardData);
 	return (
 		<ul className="mx-8 grid grid-cols-1 gap-8 md:grid-cols-2 xl:grid-cols-3 py-8">
-			{/* TODO: Fix the index */}
-			{cardData.map((card, i) => (
-				<li key={i}>
+			{cardData?.map((card) => (
+				<li key={`${card.campaign}_${card.adset}_${card.creative}`}>
 					<Card
 						campaign={card.campaign}
 						adset={card.adset}
