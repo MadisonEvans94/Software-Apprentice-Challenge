e8284dd31c1b1af29a9b89ebc89dc7a7a501c540
Madison Evans <m.corbinevans@gmail.com>
2023-08-27 09:25:46 -0700

    refactoring and reorganizing file structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import Card from "./components/Card";
-import { BsSearch as Search } from "react-icons/bs";
+import Panel from "./components/Panel";
 import { useState } from "react";
 import useDataServices from "./hooks/useDataServices";
 
@@ -41,32 +41,6 @@ function App() {
 
 export default App;
 
-const Panel = ({ setSearchQuery, setSortOrder }) => {
-	return (
-		<div className="flex border-b border-info bg-white max-h-36 w-full p-4">
-			<div className="flex flex-row my-auto mx-auto max-w-[600px] w-full">
-				<select
-					className="mr-4 border border-info rounded-lg py-2 px-4"
-					onChange={(e) => setSortOrder(e.target.value)}
-				>
-					<option value="asc">Ascending</option>
-					<option value="desc">Descending</option>
-					<option value="clear">Clear</option>
-				</select>
-				<input
-					type="text"
-					className="border border-info w-full rounded-lg py-2 px-4"
-					onChange={(e) => setSearchQuery(e.target.value)}
-					aria-label="Search Campaigns"
-				/>
-				<Search
-					size="2em"
-					className="w-10 h-full my-auto ml-4 text-info"
-				/>
-			</div>
-		</div>
-	);
-};
 const CardList = ({ cardData }) => {
 	return (
 		<ul className="mx-8 grid grid-cols-1 gap-8 md:grid-cols-2 xl:grid-cols-3 py-8">
---
+++ src/components/Panel.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { BsSearch as Search } from "react-icons/bs";
+const Panel = ({ setSearchQuery, setSortOrder }) => {
+	return (
+		<div className="flex border-b border-info bg-white max-h-36 w-full p-4">
+			<div className="flex flex-row my-auto mx-auto max-w-[600px] w-full">
+				<select
+					className="mr-4 border border-info rounded-lg py-2 px-4"
+					onChange={(e) => setSortOrder(e.target.value)}
+				>
+					<option value="asc">Ascending</option>
+					<option value="desc">Descending</option>
+					<option value="clear">Clear</option>
+				</select>
+				<input
+					type="text"
+					className="border border-info w-full rounded-lg py-2 px-4"
+					onChange={(e) => setSearchQuery(e.target.value)}
+					aria-label="Search Campaigns"
+				/>
+				<Search
+					size="2em"
+					className="w-10 h-full my-auto ml-4 text-info"
+				/>
+			</div>
+		</div>
+	);
+};
+
+export default Panel;
